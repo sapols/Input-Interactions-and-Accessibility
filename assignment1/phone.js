c0174fc1bef04ba51885fa625e077566e437f855
Shawn <sapols@yahoo.com>
2018-01-26 11:16:01 -0700

    Change tab colors upon switching in js.

--- assignment1/phone.js
+++ assignment1/phone.js
@@ -2,6 +2,7 @@ $(document).ready(function() {       //Do this when the document is loaded
     $("#dialerContent").show();      //Show the element with ID ""
     $("#contactListContent").hide(); //Hide the element with ID "contactListContent"
     $("#addContactContent").hide();
+    $("#dialerTab").css('background-color', 'white');
 });
 
 //Tab functionality
@@ -9,14 +10,23 @@ $("#dialerTab").click(function() { //When "dialerTab" is clicked
     $("#dialerContent").show();
     $("#contactListContent").hide();
     $("#addContactContent").hide();
+    $("#dialerTab").css('background-color', 'white');
+    $("#contactListTab").css('background-color', '#E6E6E6');
+    $("#addContactTab").css('background-color', '#E6E6E6');
 });
 $("#contactListTab").click(function() { //When "contactListTab" is clicked
     $("#contactListContent").show();
     $("#dialerContent").hide();
     $("#addContactContent").hide();
+    $("#contactListTab").css('background-color', 'white');
+    $("#dialerTab").css('background-color', '#E6E6E6');
+    $("#addContactTab").css('background-color', '#E6E6E6');
 });
 $("#addContactTab").click(function() { //When "addContactTab" is clicked
     $("#addContactContent").show();
     $("#dialerContent").hide();
     $("#contactListContent").hide();
+    $("#addContactTab").css('background-color', 'white');
+    $("#contactListTab").css('background-color', '#E6E6E6');
+    $("#dialerTab").css('background-color', '#E6E6E6');
 });
