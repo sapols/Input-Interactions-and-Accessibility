45c3aa2326ac05986b93735e30a26645881c8665
Shawn <sapols@yahoo.com>
2018-01-26 14:24:44 -0700

    Finish coding first iteration of the app.

--- assignment1/phone.js
+++ assignment1/phone.js
@@ -10,7 +10,8 @@ $("#dialerTab").click(function() { //When "dialerTab" is clicked
     $("#dialerContent").show();
     $("#contactListContent").hide();
     $("#addContactContent").hide();
-    $("#dialerTab").css('background-color', 'white');
+    //TODO: Turn clicked tabs to white upon no cursor hover (not after clicking elsewhere)
+    $("#dialerTab").css('background-color', 'white');  
     $("#contactListTab").css('background-color', '#E6E6E6');
     $("#addContactTab").css('background-color', '#E6E6E6');
 });
@@ -18,6 +19,7 @@ $("#contactListTab").click(function() { //When "contactListTab" is clicked
     $("#contactListContent").show();
     $("#dialerContent").hide();
     $("#addContactContent").hide();
+    //TODO: Turn clicked tabs to white upon no cursor hover (not after clicking elsewhere)
     $("#contactListTab").css('background-color', 'white');
     $("#dialerTab").css('background-color', '#E6E6E6');
     $("#addContactTab").css('background-color', '#E6E6E6');
@@ -26,6 +28,7 @@ $("#addContactTab").click(function() { //When "addContactTab" is clicked
     $("#addContactContent").show();
     $("#dialerContent").hide();
     $("#contactListContent").hide();
+    //TODO: Turn clicked tabs to white upon no cursor hover (not after clicking elsewhere)
     $("#addContactTab").css('background-color', 'white');
     $("#contactListTab").css('background-color', '#E6E6E6');
     $("#dialerTab").css('background-color', '#E6E6E6');
